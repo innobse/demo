7c69ca9f86a18c69e57f9ff02d7f323aca0adc96
Yury Penkov <bsesie71@gmail.com>
2020-09-30 19:20:52 +0300

    change hello message

--- src/main/java/ru/innopolis/university/demo/demooperators/Main.java
+++ src/main/java/ru/innopolis/university/demo/demooperators/Main.java
@@ -3,6 +3,6 @@ package ru.innopolis.university.demo.demooperators;
 public class Main {
 
   public static void main(String[] args) {
-    System.out.println("Hello, world!");
+    System.out.println("Hello, Innopolis");
   }
 }
